dd063a4525e0cb2928a82182c1894e0abc684df9
Manuel Quiroga <mquiroga@itpatagonia.com>
2024-02-16 20:26:36 -0300

    Añade paquetes, elimina componentes innecesarios, acomoda configuración de docker-compose

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Hero } from './components/Hero'
 import { MainTable } from './components/MainTable'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Toaster } from 'react-hot-toast';
+import { FilesDropdown } from './components/FilesDropdown';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,26 +13,6 @@ function App() {
     <>
       <Hero />
       <MainTable />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and sasdasdave to test
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Rto learn more
-      </p> */}
       <Toaster position="bottom-center" />
     </>
   )
