53435d3d4654c739666ad286c394b2224801680e
Manuel Quiroga <mquiroga@itpatagonia.com>
2024-02-15 21:49:35 -0300

    add test folder

---
+++ server/test/files.test.js
@@ -0,0 +1,12 @@
+import chai from "chai";
+
+const expect = chai.expect;
+
+describe('Files Controller', () => {
+
+  it('should get files list', async () => {
+    /* const response = await chai.request('http://localhost:3000').get('/files/list');
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.an('array'); */
+  });
+})
