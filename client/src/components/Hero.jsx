fb6db19775bec0f66e1969ba82c5e426d145764e
Manuel Quiroga <mquiroga@itpatagonia.com>
2024-02-16 21:26:55 -0300

    Adds responsive image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,10 +1,21 @@
-import cleanhero from '../assets/cleanhero.webp'
-import './Hero.css'
+import cleanhero from '../assets/cleanhero.webp';
+import smallhero from '../assets/smallhero.webp';
+import './Hero.css';
 
 export function Hero() {
-    return (
-        <section className="Hero">
-            <img className="hero-image" src={cleanhero} alt="clean hero"></img>
-        </section>
-    )
+  return (
+    <section className="Hero">
+      <picture>
+        <source 
+          media="(min-width: 1020px)"
+          srcSet={cleanhero} 
+        />
+        <img 
+          srcSet={smallhero}
+          alt="hero image"
+        />
+      </picture>
+    </section>
+  )
 }
+
